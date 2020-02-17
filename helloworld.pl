00fd6dffc6ccf3519ea81d4786c847a5a18a9149
RajuLingappagari <44128813+RajuLingappagari@users.noreply.github.com>
2020-02-17 18:26:43 +0530

    Update helloworld.pl

--- helloworld.pl
+++ helloworld.pl
@@ -4,3 +4,9 @@
 print "Hello, world\n";
 print "Welcome to PERL Learning\n";
 print "My name Raju I started Learning Perl";
+
+=begin output
+Hello, world
+Welcome to PERL Learning
+My name Raju I started Learning Perl
+=cut
