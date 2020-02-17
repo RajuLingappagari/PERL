c2d5c17e86185ef2c013800387d75de6318d4617
RajuLingappagari <44128813+RajuLingappagari@users.noreply.github.com>
2020-02-17 18:25:20 +0530

    Create single_double_quotes.pl

---
+++ single_double_quotes.pl
@@ -0,0 +1,15 @@
+#!/usr/bin/perl
+
+$a=10;
+
+#Difference between single and Double Quotes
+
+print "Vlue of a = $a\n";
+print 'Value of a = $a\n';
+
+
+=begin output 
+output:
+Vlue of a = 10
+Value of a = $a\n
+=cut
